aa5e53ab5535e38f293e7148ae030fff74c28895
mgbrouli <47576364+mgbrouli@users.noreply.github.com>
2023-04-18 16:38:40 -0300

    New teste
    new Project to pdv teste

---
+++ app.py
@@ -0,0 +1,21 @@
+class cadastro:
+    def __init__(self, codBarra, nomeProd, marca, medida, balanca, ncm, cest):
+        
+        codBarra =0000000000000
+        nomeProd ="nome"
+        marca = "marca"
+        medida = "unid"
+        balanca = True
+
+        departamento = ''
+        secao = ''
+        grupo = ''
+        subgrupo = ''
+        ncm = 00000000
+        cest = 00000000
+        
+        pass
+
+produto1 = cadastro(7890001112223, "teste", "nestle", "unid", False, 21351235, 22331156 )
+
+print(produto1)
